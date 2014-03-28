41fddefb65897023b2907b632ca89e086f790ade
Kevin Smith <smith.ks64@gmail.com>
2014-03-28 09:37:14 -0400

    add db entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.create(name:'Alexander Valley', year:2011, winery:'SIMI', country:'USA', varietal:'Cabernet Sauvignon')
+Wine.create(name:'Merlot Reserve', year:2011, winery:'Barrel Oak', country:'USA', varietal:'Merlot')
+Wine.create(name:'Sonoma County', year:2012, winery:'SIMI', country:'USA', varietal:'Chardonnay')
