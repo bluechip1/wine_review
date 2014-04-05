f862e22fe4ec4ab5805eeea2a37e98f9a2bf71c4
Kevin Smith <smith.ks64@gmail.com>
2014-04-05 14:42:33 -0400

    homwork_9

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,10 +1,42 @@
 class WinesController < ApplicationController
+  before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+    
   def index
   	@wines = Wine.all
   	@available_at = Time.now
   end
 
   def show
-  	@wine = Wine.find(params[:id])
+  end
+
+  def new
+  	@wine = Wine.new
+  end
+
+  def create
+  	@wine = Wine.new(wine_params)
+    @wine.save
+    redirect_to @wine
+  end
+
+  def edit
+  end
+
+  def update
+  	@wine.update(wine_params)
+  	redirect_to @wine
+  end
+
+  def destroy
+  	@wine.destroy
+    redirect_to wines_url
+  end
+
+  def set_wine
+    @wine = Wine.find(params[:id])
+  end
+
+  def wine_params
+      params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
   end
 end
