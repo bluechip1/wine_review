008f7cf71a62fe3aaf4a56fcbceb153649c703f3
Kevin Smith <smith.ks64@gmail.com>
2014-04-21 07:31:29 -0400

    homework 10d

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -1,32 +1,32 @@
 class LogEntriesController < ApplicationController
 	before_action :set_wine
 
-		def index
-		  @log_entries = @wine.log_entries.order('created_at desc')
-		end
+	def index
+	  @log_entries = @wine.log_entries.order('created_at desc')
+	end
 
-		def new
-		  @log_entries = @wine.log_entries.new
-		end
+	def new
+	  @log_entry = @wine.log_entries.new
+	end
 
-		def create
-		  @log_entry = @wine.log_entries.new(log_entry_params)
-		  if @log_entry.save
-		    redirect_to wine_log_entryies_path(@wine), notice: 'Log Entry saved!'
-		  else
-		    render :new
-		  end
-		end
+	def create
+	  @log_entry = @wine.log_entries.new(log_entry_params)
+	  if @log_entry.save
+	    redirect_to wine_log_entries_path(@wine), notice: 'Log Entry saved!'
+	  else
+	    render :new
+	  end
+	end
 
-		private
+	private
 
-		def set_wine
-		  @wine = Wine.find(params[:wine_id])
-		end
+	def set_wine
+	  @wine = Wine.find(params[:wine_id])
+	end
 
-		private
+	private
 
-		def  log_entry_params
-		  params.require(:log_entry).permit(:name, :rating, :location, :comments, :tasted_on)
-		end
+	def  log_entry_params
+	  params.require(:log_entry).permit(:name, :rating, :location, :comments, :tasted_on)
+	end
 end
--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -1,4 +1,5 @@
 class LogEntry < ActiveRecord::Base
   RATINGS = 1..5
   belongs_to :wine
+    validates :name, :rating, :location, :comments, :tasted_on, presence: true
 end
